ee93e2236dfc373f64610e2c783116431db0da92
SaraHussein28 <85261712+SaraHussein28@users.noreply.github.com>
2022-03-17 17:12:03 +0200

    Create Watermelon.java

--- Watermelon.java
+++ Watermelon.java
@@ -2,8 +2,7 @@ import java.util.Scanner;
 
 public class Watermelon {
 
-    public static void main (String[] args) throws java.lang.Exception
-    {
+    public static void main(String[] args) throws java.lang.Exception {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         if (a > 2 && a % 2 == 0)
@@ -11,4 +10,11 @@ public class Watermelon {
         else System.out.print("NO");
     }
 
+    public String divide(int a) {
+        if (a > 2 && a % 2 == 0)
+            return ("YES");
+        return ("NO");
     }
+
+
+}
